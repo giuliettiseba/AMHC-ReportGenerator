eec9e82e7752ebb2e14de117a66ae0abaae1ed5c
Sebastian Giulietti <giuliettiseba@gmail.com>
2022-04-05 10:00:09 -0500

    Initial Commint

--- AMHC-ReportGenerator/Form1.Designer.cs
+++ AMHC-ReportGenerator/Form1.Designer.cs
@@ -29,13 +29,34 @@ namespace AMHC_ReportGenerator
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button_CreateReport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_CreateReport
+            // 
+            this.button_CreateReport.Location = new System.Drawing.Point(235, 260);
+            this.button_CreateReport.Name = "button_CreateReport";
+            this.button_CreateReport.Size = new System.Drawing.Size(322, 23);
+            this.button_CreateReport.TabIndex = 0;
+            this.button_CreateReport.Text = "Create Report";
+            this.button_CreateReport.UseVisualStyleBackColor = true;
+            this.button_CreateReport.Click += new System.EventHandler(this.button_CreateReport_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button_CreateReport);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_CreateReport;
     }
 }
 
